6eb8a13deea4ed7591ec6a90c03355e8dd0b9a30
Gokulcasper <ronitcasper215@gmail.com>
2022-10-05 20:51:07 +0530

    Feat : Crud Operation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import Create from './Component/Create';
+import Read from './Component/Read';
+import Update from './Component/Update';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>CRUD Operation</h2>
+
+
+
+      <BrowserRouter>
+        <Routes>
+          <Route exact path='/create' element={<Create />} />
+          <Route exact path='/update' element={<Update />} />
+          <Route exact path='/read' element={<Read />} />
+        </Routes>
+      </BrowserRouter>
+
     </div>
   );
 }
